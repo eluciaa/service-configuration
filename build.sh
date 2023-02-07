154427bff437285f83a7473714deae9f677add72
Eber Calderon <67397815+ebercalderon@users.noreply.github.com>
2023-02-06 21:42:12 -0500

    build

--- build.sh
+++ build.sh
@@ -1,27 +1,9 @@
 #!/bin/bash
 
-microservices=("config-server" "eureka-server" "gateway" "customer-microservice" "movement-microservice" "passive-microservice")
+microservices=("ms-configserver" "ms-eurekaserver" "ms-gateway" "ms-customer" "ms-movement" "ms-credit" "ms-bankaccount" "ms-statusaccount")
 cd ..
 
 # Profile
-read -p "Do you want to change the active profile? (Y/N) " answer
-
-if [ "$answer" == "Y" ] || [ "$answer" == "y" ]; then
-  read -p "Enter the value for the active profile (dev, prod, or docker): " value
-  if [ "$value" == "dev" ] || [ "$value" == "prod" ] || [ "$value" == "docker" ]; then
-    for microservice in "${microservices[@]}"; do
-      if [ -d "$microservice" ]; then
-        bootstrap_file="$microservice/src/main/resources/bootstrap.yml"
-        if [ -f "$bootstrap_file" ]; then
-          sed -i "5s/active:.*/active: $value/" "$bootstrap_file"
-        fi
-      fi
-    done
-  else
-    echo "Invalid value entered. Exiting script."
-    exit 1
-  fi
-fi
 
 #Jar
 read -p "Do you want to generate the jar files? (Y/N) " answer
@@ -43,7 +25,7 @@ fi
 read -p "Do you want to start the Docker containers? (Y/N) " answer
 if [ "$answer" == "Y" ] || [ "$answer" == "y" ]; then
   # Start the Docker containers
-  cd ./config-server-directory
+  cd ./service-configuration
   docker-compose up --build
 else
   echo "Exiting without starting Docker containers"
